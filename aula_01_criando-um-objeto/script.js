eba67fae8c42854372309892385d1e17b1729fc8
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-02-25 22:38:31 -0300

    Lesson 01: Creating an Object

---
+++ aula_01_criando-um-objeto/script.js
@@ -0,0 +1,29 @@
+/*
+    OBJETO
+        - Uma coleção de dados e/ou funcionalidades;
+        - Podem ter propriedades e métodos;
+*/
+
+// Cria um objeto vazio.
+const obj = {}
+console.log(obj);
+console.log(typeof obj);
+
+// Cria um objeto com propriedadres e métodos.
+const user = {
+    email: "pedro@email.com",
+    age: 38,
+    name: {
+        first_name: "Pedro",
+        surname: "Carvalho",
+    },
+    adress: {
+        street: "Rua X",
+        number: 23,
+        city: "São Paulo",
+        postal_code: "12345-123"
+    },
+    message: () => {
+        console.log("Olá, Pedro")
+    }
+}
