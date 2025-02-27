5443cd8ecc0d18d5ec847dc2cb94e97a0c21c66f
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-02-26 21:43:01 -0300

    lesson 04: Updating an object

---
+++ aula_04_atualizando-objeto/script.js
@@ -0,0 +1,22 @@
+/*
+    OBJETO
+        - Atualizando um objeto
+*/
+
+const product = {
+    name: "Teclado",
+    quantity: 100,
+}
+
+// Atualiza o valor de uma propriedade
+console.log(product.quantity)
+product.quantity = 90
+console.log(product.quantity)
+
+console.log(product.name)
+product.name = "Mouse"
+console.log(product.name)
+
+// Notação de colchetes
+product["quantity"] = 50
+console.log(product)
