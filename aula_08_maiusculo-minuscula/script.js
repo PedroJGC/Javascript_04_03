8f380812edcc91e7449a3a2263aa372139e24a34
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-02-27 21:31:51 -0300

    Lesson 08: Uppercase and  Lowercase

---
+++ aula_08_maiusculo-minuscula/script.js
@@ -0,0 +1,13 @@
+/*
+    String
+        - Uppercase
+        - Lowercase
+*/
+
+let message = "Estou estudando os fundamentos do Javascript."
+
+// Uppercase
+console.log(message.toUpperCase())
+
+// Lowercase
+console.log(message.toLowerCase())
