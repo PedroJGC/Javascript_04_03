90abcd1ea3fe70559babda4dac489b0812d3542b
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-01 17:38:19 -0300

    Lesson 14: Creating array with a constructor

---
+++ aula_14_criando-um-array-com-construtor/script.js
@@ -0,0 +1,16 @@
+/*
+    Array
+        - array com construtor 
+*/
+
+ // Criando Array com construtor
+ const newArray = new Array()
+ console.log(newArray)
+
+ // Retorna o tamanho do Array
+ console.log(newArray.length)
+
+ // Cria um Array com 10 posições vazias
+ const available = new Array(10)
+ console.log(available) 
+ console.log(available.length) 
