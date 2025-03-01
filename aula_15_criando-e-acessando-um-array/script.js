c368394ee686c0adc813330fdc8d975a220aca8d
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-01 18:01:16 -0300

    Lesson 15: Creating and accessing an array

---
+++ aula_15_criando-e-acessando-um-array/script.js
@@ -0,0 +1,20 @@
+/*
+    Array
+        - Criando e acessando um array
+*/
+
+// Cria o Array
+let fruits = ["Apple", "Banana", "Orange"]
+console.log(fruits)
+
+// Quantidade de itens do array
+console.log(fruits.length)
+
+// Acessa o item pelo índice
+console.log(fruits[0])
+
+// Tenta acessar um índice que não existe (undefined)
+console.log(fruits[7])
+
+// Obtém dinamicamente o último item
+console.log(fruits[fruits.length - 1])
