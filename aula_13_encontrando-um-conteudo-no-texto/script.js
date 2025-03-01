fcb26cc2e19f114e74b46956f2ada06bce9f1b6e
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-01 17:13:34 -0300

    Lesson 13: indexOf, includes

---
+++ aula_13_encontrando-um-conteudo-no-texto/script.js
@@ -0,0 +1,20 @@
+/*
+    String
+        - indexOf
+        - includes
+*/
+
+let message = "Estou estudando os fundamentos do Javascript."
+
+// Obtém a posição da palavra
+console.log(message.indexOf("Javascript"))
+
+// Quando não encontra retorna -1
+// console.log(message.indexOf("javascript"))
+
+// Verifica se existe a palavra na String
+console.log(message.includes("Javascript")) // true
+
+console.log(message.includes("javascript")) // false
+console.log(message.toLocaleLowerCase().includes("javascript")) // true
+
