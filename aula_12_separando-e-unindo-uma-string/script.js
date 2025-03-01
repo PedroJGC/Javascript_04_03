a1293b1a45b1e72c9a98a30acc077dfeae632956
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-02-28 22:05:27 -0300

    Lesson 12: Split and Join

---
+++ aula_12_separando-e-unindo-uma-string/script.js
@@ -0,0 +1,16 @@
+/*
+    String
+        - split
+        - join
+*/
+
+let text = "Estudar, Aprender, Praticar"
+
+let separate = text.split(",")
+console.log(separate)
+
+let message = "Estou aprendendo Javascript"
+console.log(message.split(" "))
+
+let joined = separate.join(" -")
+console.log(joined)
