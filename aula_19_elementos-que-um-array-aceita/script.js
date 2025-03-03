3a7f1047fd8fe366aef1de701efbc9af409decdc
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-03 19:41:34 -0300

    Lesson 19: Elements that an array accepts

---
+++ aula_19_elementos-que-um-array-aceita/script.js
@@ -0,0 +1,32 @@
+/*
+    Array
+        - Quais elementos que um array aceita
+*/
+
+let myArray = [
+    "Um texto",
+    10,
+    true,
+    function() {
+        console.log("função dentro o Array!")
+    },
+    {
+        name: "Pedro",
+        email: "pedro@email.com",
+    },
+]
+
+// Texto
+console.log(myArray[0])
+
+// Número
+console.log(myArray[1])
+
+// Boolean
+console.log(myArray[2])
+
+// Função
+myArray[3]()
+
+// Objeto
+console.log(myArray[4].name, myArray[4].email)
