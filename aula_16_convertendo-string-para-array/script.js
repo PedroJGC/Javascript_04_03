d749016b2a4b8979a2d18ca67c0d13f32f5d5d99
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-03 18:33:19 -0300

    Lesson 16: Convert string to array

---
+++ aula_16_convertendo-string-para-array/script.js
@@ -0,0 +1,12 @@
+/*
+    Array
+        - Convertendo uma String para array
+*/
+
+let fullName = "Pedro Julio Guimarães"
+
+// Cria um array com os nomes separados pelo espaço.
+console.log(fullName.split(" "))
+
+// Cria um array com as letras.
+console.log(Array.from(fullName))
