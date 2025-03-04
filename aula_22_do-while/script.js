81f7dba719a333e770e293bb0c5333e9d41eee4d
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-04 11:56:16 -0300

    Lesson 22: Do While

---
+++ aula_22_do-while/script.js
@@ -0,0 +1,15 @@
+/*
+    DO WHILE
+        - Repete até que a condição especificada seja VERDADEIRA.
+        No entanto, a instrução será executada pelo menos uma vez
+        antes da condição ser verificada.
+*/
+
+let value = 0;
+
+do {
+  value++;
+  console.log(value);
+} while (value < 10);
+
+console.log('Segue o fluxo...');
