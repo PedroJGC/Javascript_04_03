377c1fa6f31444f96f0c3dc226aaf3cf833b7435
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-05 20:03:04 -0300

    Lesson 23: For

---
+++ aula_23_for/script.js
@@ -0,0 +1,23 @@
+/*
+    For
+        - Repete até que a condição especificada seja Falsa.
+        
+        - variável de controle
+        - condição
+        - incremento ou decremento da variável de controle
+*/
+
+/*
+for (step = 0; step < 10; step++) {
+  console.log(step);
+}
+*/
+
+// Exemplo de tabuada.
+console.log('#####################');
+
+let number = 7;
+
+for (step = 0; step <= 10; step++) {
+  console.log(`${number} x ${step} = ${number * step}`);
+}
