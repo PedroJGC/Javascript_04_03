4743ba707343242187b78585c247488981885965
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-05 20:49:42 -0300

    Lesson 25: For of

---
+++ aula_25_for-of/script.js
@@ -0,0 +1,10 @@
+/*
+    For of
+        - Itera sobre valores de um objeto iterável.
+*/
+
+let students = ['Pedro', 'Amanda', 'John'];
+
+for (let student of students) {
+  console.log(student);
+}
