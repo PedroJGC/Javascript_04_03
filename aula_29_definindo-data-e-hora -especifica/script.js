63f7c09d10849f2edad06506b8478fbc64411720
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-06 19:08:04 -0300

    Lesson 29: Setting specific date and time

---
+++ aula_29_definindo-data-e-hora -especifica/script.js
@@ -0,0 +1,14 @@
+/*
+    Definindo data e hora especifíca
+*/
+
+// Define com ano, mês (0-11), dia.
+console.log(new Date(2024, 6, 2));
+
+// Define ano, mês (0-11), dia e hora, minuto e segundo.
+console.log(new Date(2024, 6, 2, 14, 30, 0));
+
+// Definindo data e hora com String
+console.log(new Date('2024-07-02T14:30:00'));
+
+console.log(new Date('July 2, 2032 13:30:33'));
