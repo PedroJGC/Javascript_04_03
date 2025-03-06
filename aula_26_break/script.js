9bf2590d363c562bb2150af164059d84f7362ea5
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-06 18:15:05 -0300

    Lesson 26: Break

---
+++ aula_26_break/script.js
@@ -0,0 +1,12 @@
+/*
+    Break 
+        - Encerra a execução da repetição ou switch para
+        seguir a instrução seguinte.
+*/
+
+for (i = 0; i <= 10; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log(i);
+}
