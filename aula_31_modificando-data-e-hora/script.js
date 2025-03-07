b7d9f55bc9d55ef5e56adf800e60358022123d41
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-07 18:02:16 -0300

    Lesson 31: Modifying date and time

---
+++ aula_31_modificando-data-e-hora/script.js
@@ -0,0 +1,25 @@
+/*
+    Modificando data e hora.
+*/
+
+let date = new Date('July 3, 2025 14:30:00');
+
+// Modificar o ano.
+date.setFullYear(2030);
+
+// Modificar o mês.
+date.setMonth(1);
+
+// Modificar o dia.
+date.setDate(10);
+
+// Modificar a hora.
+date.setHours(18);
+
+// Modificar os minutos.
+date.setMinutes(15);
+
+// Modificar os segundos.
+date.setSeconds(30);
+
+console.log(date);
